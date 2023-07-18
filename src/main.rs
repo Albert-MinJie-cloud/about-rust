31d634a8f9bef57f6da6a36a95f7c3f8e7e102b6
闵杰 <minjie@minjiedeMacBook-Pro-4.local>
2023-07-19 00:29:51 +0800

    feat:初始化

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("猜数字");
+
+    println!("请输入数字！");
+
+    // let mut foo = 1;
+    // let bar = foo;
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("无法读取行");
+
+    println!("你猜测的数字是{}", guess);
 }
